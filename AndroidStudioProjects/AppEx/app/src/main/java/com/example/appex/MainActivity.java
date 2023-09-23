35a78906c8b8dbb133b27f2a3e84632f289f0e25
Akash <akashbiswaliit7@gmail.com>
2023-09-23 18:32:16 +0530

    project initialised

---
+++ AndroidStudioProjects/AppEx/app/src/main/java/com/example/appex/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.appex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
